0986655015cece2192f38f1cf1cfef86c67eee85
Arif Hoque Sarkar <120245077+Arifhoque1999@users.noreply.github.com>
2023-03-25 12:07:12 +0530

    frist

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    console.log("constructor 1");
+    this.state = { count: 0 };
+  }
+
+  componentDidMount() {
+    console.log("componentDidMount 2");
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    console.log("componentDidUpdate");
+    console.log("prevProps", prevProps);
+    console.log("prevState", prevState);
+    console.log("PROPS", this.props);
+    console.log("State", this.state);
+    if (prevState.count === 0 && this.state.count === 1) {
+      this.setState({ count: 100 });
+    }
+  }
+
+  componentWillUnmount(){
+    // cleanUp
+  }
+
+  handelCkick = () => {
+    this.setState((prevState) => {
+      return {
+        count: prevState.count + 1,
+      };
+    });
+  };
+  render() {
+    console.log("Render 3");
+    return (
+      <div className="App">
+        <h1>{this.state.count}</h1>
+        <button onClick={this.handelCkick}> onClick</button>
+      </div>
+    );
+  }
 }
 
 export default App;
